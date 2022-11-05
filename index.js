9c1b1bb20755141e1c8a4710b6ef22e508775d6f
Sneaky Panda <sneakypanda99@proton.me>
2022-11-05 14:51:09 +0100

    Patch

--- index.js
+++ index.js
@@ -4,10 +4,70 @@ const { ipcMain, Tray, app, BrowserWindow } = require("electron");
 var isDev = require('electron-is-dev')
 const path = require("path");
 const setupEvents = require('./installers/setupEvents')
-require('update-electron-app')({
-  repo: 'sneakypanda99/PengKlocka90',
-  updateInterval: '1 hour',
-})
+const squirrelUrl = "http://10.80.72.11:3333";
+const electron = require('electron');
+var request = require('request');
+
+
+
+var link = squirrelUrl;
+
+
+
+const checkIfAlive = () => {return request(`${link}/releases?` , function (error, response, body) {
+
+  if(error){
+    console.log('Err: '+ error);
+    return false;
+  }
+
+if(response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202){
+  console.log(link + ' is up!!');
+  electron.autoUpdater.checkForUpdates();
+  return true;
+}
+
+if(response.statusCode == 301 || response.statusCode == 302){
+  console.log(link + ' is redirecting us!!');
+  return false;
+}
+
+if(response.statusCode == 401){
+  console.log("you are unauthorized to " + link);
+  return false;
+
+}else{
+  console.log(link + ' is down!!');
+  return false
+}
+
+});}
+
+
+const startAutoUpdater = (squirrelUrl) => {
+  // The Squirrel application will watch the provided URL
+  electron.autoUpdater.setFeedURL(`${squirrelUrl}/`);
+
+  // Display a success message on successful update
+  electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
+    electron.dialog.showMessageBox({"message": `The release ${releaseName} has been downloaded`});
+  });
+
+  // Display an error message on update error
+  electron.autoUpdater.addListener("error", (error) => {
+    electron.dialog.showMessageBox({"message": "Auto updater error: " + error});
+  });
+
+  // tell squirrel to check for updates
+  try {
+    checkIfAlive()
+  }
+  catch {
+    return null
+  }
+}
+
+
 
 if (setupEvents.handleSquirrelEvent()) {
    // squirrel event handled and app will exit in 1000ms, so don't do anything else
@@ -25,7 +85,12 @@ if(!isDev) {
 
 app.on("ready", () => {
   
- 
+  if(!isDev) {
+    startAutoUpdater(squirrelUrl)
+    setInterval(() => {
+      startAutoUpdater(squirrelUrl)
+  }, 60*1000*15)
+  }
   var timeout = 10;
   if (process.platform === "linux") {
     timeout = 200;
