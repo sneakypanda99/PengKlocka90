2b29ec17dbca0c059fb3002b1cfe0f6460fc874c
Sneaky Panda <sneakypanda99@proton.me>
2023-05-02 10:06:45 +0200

    fix: imrpoved accuracy

--- index.js
+++ index.js
@@ -1,95 +1,89 @@
 const TrayWindow = require("electron-tray-window");
 const ipc = require("electron").ipcMain;
 const { ipcMain, Tray, app, BrowserWindow } = require("electron");
-var isDev = require('electron-is-dev')
+var isDev = require("electron-is-dev");
 const path = require("path");
-const setupEvents = require('./installers/setupEvents')
-const squirrelUrl = "http://10.80.72.11:3333";
-const electron = require('electron');
-var request = require('request');
-
-
+const setupEvents = require("./installers/setupEvents");
+const squirrelUrl = "http://update.tok1.se:3333";
+const electron = require("electron");
+var request = require("request");
 
 var link = squirrelUrl;
 
+const checkIfAlive = () => {
+  return request(`${link}/releases?`, function (error, response, body) {
+    if (error) {
+      console.log("Err: " + error);
+      return false;
+    }
 
+    if (
+      response.statusCode == 200 ||
+      response.statusCode == 201 ||
+      response.statusCode == 202
+    ) {
+      console.log(link + " is up!!");
+      electron.autoUpdater.checkForUpdates();
+      return true;
+    }
 
-const checkIfAlive = () => {return request(`${link}/releases?` , function (error, response, body) {
+    if (response.statusCode == 301 || response.statusCode == 302) {
+      console.log(link + " is redirecting us!!");
+      return false;
+    }
 
-  if(error){
-    console.log('Err: '+ error);
-    return false;
-  }
-
-if(response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202){
-  console.log(link + ' is up!!');
-  electron.autoUpdater.checkForUpdates();
-  return true;
-}
-
-if(response.statusCode == 301 || response.statusCode == 302){
-  console.log(link + ' is redirecting us!!');
-  return false;
-}
-
-if(response.statusCode == 401){
-  console.log("you are unauthorized to " + link);
-  return false;
-
-}else{
-  console.log(link + ' is down!!');
-  return false
-}
-
-});}
-
+    if (response.statusCode == 401) {
+      console.log("you are unauthorized to " + link);
+      return false;
+    } else {
+      console.log(link + " is down!!");
+      return false;
+    }
+  });
+};
 
 const startAutoUpdater = (squirrelUrl) => {
   // The Squirrel application will watch the provided URL
   electron.autoUpdater.setFeedURL(`${squirrelUrl}/`);
 
   // Display a success message on successful update
-  electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
-    electron.autoUpdater.quitAndInstall()
-  });
+  electron.autoUpdater.addListener(
+    "update-downloaded",
+    (event, releaseNotes, releaseName) => {
+      electron.autoUpdater.quitAndInstall();
+    }
+  );
 
   // Display an error message on update error
   electron.autoUpdater.addListener("error", (error) => {
-    electron.dialog.showMessageBox({"message": "Auto updater error: " + error});
+    electron.dialog.showMessageBox({ message: "Auto updater error: " + error });
   });
 
   // tell squirrel to check for updates
   try {
-    checkIfAlive()
+    checkIfAlive();
+  } catch {
+    return null;
   }
-  catch {
-    return null
-  }
-}
-
-
+};
 
 if (setupEvents.handleSquirrelEvent()) {
-   // squirrel event handled and app will exit in 1000ms, so don't do anything else
-   return;
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
 }
 
-
-
-if(!isDev) {
+if (!isDev) {
   app.setLoginItemSettings({
-    openAtLogin:true
-  })
+    openAtLogin: true,
+  });
 }
-
 
 app.on("ready", () => {
-  
-  if(!isDev) {
-    startAutoUpdater(squirrelUrl)
+  if (!isDev) {
+    startAutoUpdater(squirrelUrl);
     setInterval(() => {
-      startAutoUpdater(squirrelUrl)
-  }, 60*1000*15)
+      startAutoUpdater(squirrelUrl);
+    }, 60 * 1000 * 15);
   }
   var timeout = 10;
   if (process.platform === "linux") {
@@ -97,11 +91,10 @@ app.on("ready", () => {
   }
   setTimeout(function () {
     TrayWindow.setOptions({
-      trayIconPath:`${path.join(__dirname, "/shrek.ico")}`,
+      trayIconPath: `${path.join(__dirname, "/shrek.ico")}`,
       windowUrl: `file://${path.join(__dirname, "resources/index.html")}`,
       width: 340,
       height: 350,
     });
   }, timeout);
 });
-
--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -1,27 +1,27 @@
-const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
-const path = require('path')
+const createWindowsInstaller =
+  require("electron-winstaller").createWindowsInstaller;
+const path = require("path");
 
 getInstallerConfig()
   .then(createWindowsInstaller)
   .catch((error) => {
-    console.error(error.message || error)
-    process.exit(1)
-  })
+    console.error(error.message || error);
+    process.exit(1);
+  });
 
-function getInstallerConfig () {
-  console.log('creating windows installer')
-  const rootPath = path.join('./')
-  const outPath = path.join(rootPath, 'release-builds')
+function getInstallerConfig() {
+  console.log("creating windows installer");
+  const rootPath = path.join("./");
+  const outPath = path.join(rootPath, "release-builds");
 
   return Promise.resolve({
-    appDirectory: path.join(outPath, 'PengKlocka90-win32-x64/'),
-    authors: 'VPL Thörnblom',
+    appDirectory: path.join(outPath, "PengKlocka90-win32-x64/"),
+    authors: "VPL Thörnblom",
     noMsi: true,
-    outputDirectory: path.join(outPath, 'windows-installer'),
-    exe: 'PengKlocka90.exe',
-    setupExe: 'PengKlocka90.exe',
-    setupIcon: path.join(rootPath, 'shrek.ico'),
-    loadingGif: path.join(rootPath, 'shrek.gif'),
-
-  })
+    outputDirectory: path.join(outPath, "windows-installer"),
+    exe: "PengKlocka90.exe",
+    setupExe: "PengKlocka90.exe",
+    setupIcon: path.join(rootPath, "shrek.ico"),
+    loadingGif: path.join(rootPath, "shrek.gif"),
+  });
 }
